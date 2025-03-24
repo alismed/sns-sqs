d7bd30ff23299cdd987dbe79da97549ce7aabc28
Alison Souza <alisonmsouza@yahoo.com.br>
2025-03-23 21:18:05 -0300

    backend

---
+++ infra/backend.local.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket                      = "alismed-terraform"
+    key                         = "terraform.tfstate"
+    region                      = "us-east-1"
+    endpoint                    = "http://localhost:4566"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style           = true
+    profile                    = "localstack"
+  }
+}
--- infra/backend.tf
+++ infra/backend.tf
@@ -1,12 +1,7 @@
 terraform {
   backend "s3" {
-    bucket                      = local.backend_config.bucket
-    key                         = local.backend_config.key
-    region                      = local.backend_config.region
-    endpoint                    = local.backend_config.endpoint
-    skip_credentials_validation = local.backend_config.skip_credentials_validation
-    skip_metadata_api_check     = local.backend_config.skip_metadata_api_check
-    force_path_style            = local.backend_config.force_path_style
-    profile                     = local.backend_config.profile
+    bucket = "alismed-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-1"  // Must be a static value
   }
 }
